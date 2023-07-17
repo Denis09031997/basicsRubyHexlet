0b1d1b4fab214aa6cc333b31adda5004c9449072
Denis <den.scherbinin2018@yandex.ru>
2023-07-17 23:26:48 +0500

    finish learn 10

--- lesson10practic.rb
+++ lesson10practic.rb
@@ -1,2 +1,12 @@
 #Реализуйте функцию show_me_numbers(), которая выводит на экран числа от одного до переданного в функцию
 # в обратном порядке:
+
+def show_me_numbers(n)
+  (1..n).reverse_each do |num|
+    puts num
+  end
+end
+
+
+
+puts show_me_numbers(5)
